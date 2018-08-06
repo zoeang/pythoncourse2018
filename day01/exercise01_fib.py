975aec18aa19535930c1c5ff2eb283e40cab3896
zoeang <36051346+zoeang@users.noreply.github.com>
2018-08-06 11:40:32 -0500

    Zoe's fib while loop

--- day01/exercise01_fib.py
+++ day01/exercise01_fib.py
@@ -4,3 +4,20 @@
 
 ## Write a for loop, while loop, or function (or all three!) to create a
 ## list of the first 10 numbers of the fibonacci sequence
+
+#this will be recursive, so start with the first element?
+#fib[0]: #1
+
+#while loop
+fib=[1]
+while len(fib)<11:
+   if len(fib)<2:
+      fib.append(1)
+   else:
+      fib.append(fib[-1]+fib[-2]) #append the next element
+      print fib
+
+# for loop
+#does not work
+fib_loop = [i+(i-1) for i-1 in range(1,10)]
+
