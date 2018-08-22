5e007cd15658dbb3476a15f1f701e596caada08e
zoeang <36051346+zoeang@users.noreply.github.com>
2018-08-22 14:01:54 -0500

    merge sort alg complete

--- homeworks/hw4/Ang_HW4.py
+++ homeworks/hw4/Ang_HW4.py
@@ -2,7 +2,7 @@
 # Homework 4
 #====================================================================
 #Sorting Algorithms
-# Algorithm 1: Bubble sorting; best case O(n log n)
+# Algorithm 1: Bubble sorting; best caseO(n), worst case O(n^2)
 
 def bubble(n): # n is a list
 	for i in range(0, len(n)-1):
@@ -13,10 +13,42 @@ def bubble(n): # n is a list
 		else: pass
 	return n
 
+# Algorithm 2: Merge sorting ; best and worst case O(n log n) ---------------------------------
+# Need help debugging: dropping first element of arugment list
+#====================================================================
+def merge(left, right):
+	output=[]
+	while len(left)>0 and len(right)>0:
+		if left[0]<=right[0]:
+			output.append(left[0])
+			left.remove(left[0])
+		else: 
+			output.append(right[0])
+			right.remove(right[0])
+	#there will be one element remaining in either the left or right list
+	#the loop relating to the list with the final element will be entered
+	if len(left)>0:
+		output.append(left[0])
+		#print 'left list'
+	if len(right)>0:
+		output.append(right[0])
+		#print 'rightlist'
+	return output ###says that there should be an indent here, but there shouldn't be...
 
-# Algorithm 2: Merge sorting ; best case O(n)---------------------------------
-	# break into dyads
-
+def merge_sort(l):
+	if len(l)<=1:
+		return l
+	left=[]
+	right=[]
+	for i in range(len(l)):
+		if i%2!=0:
+			left.append(l[i])
+		else: right.append(l[i])
+	leftlist=merge_sort(left)
+	#print str(leftlist) +'\t left'
+	rightlist=merge_sort(right)
+	#print str(rightlist) + '\t right'
+	return merge(leftlist, rightlist)
 # Funtion to time algorithms---------------------------------------------
 
 # Returns tuple of time and length of arg and time (tuple will be (x,y) point for ploting)
@@ -26,3 +58,16 @@ def timealg(func,arg): #func is the name of the function, arg is the input to th
 	func(arg)
 	end=timeit.default_timer()
 	return len(arg), end-start
+
+#generate data to plot
+#the first list returned is the bubble alg, the second, merge sorting alg
+from random import uniform 
+lis=[1]
+def dat(lis):
+	bub=[]
+	merge_alg=[]
+	while len(lis)<100:
+		bub.append(timealg(bubble,lis))
+		merge_alg.append(timealg(merge_sort, lis))
+		lis.append(int(uniform(1,100)))
+	return [bub, merge_alg]
